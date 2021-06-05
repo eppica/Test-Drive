22cb4585061633750e251b400bffe0017dde104f
Pedro Henrique Ferreira <pedrohfcd@hotmail.com>
2021-06-05 17:18:55 -0300

    Implements ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+	'env': {
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module',
+		'project': './tsconfig.json',
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint',
+		'react-hooks'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'no-empty-function': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'react/display-name': 'off',
+		'react/prop-types': 'off'
+	},
+	'settings': {
+	  'react': {
+		'version': 'detect',
+	  },
+	}
+};
