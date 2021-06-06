89011c521e0ca2ed9b3583d2b4f2246782f1923b
LBeghini <lavinia.beghini@gmail.com>
2021-06-06 17:57:06 -0300

    Revert "Create stack navigation"
    This reverts commit bc2d35ec22a8a22694f43c81c277210ec247d0a2.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { ConteudoStack } from './navigation/conteudoStack';
-import { NavigationContainer } from '@react-navigation/native';
 
-import Conteudo from './screens/Conteudo';
-import Teste from './screens/Teste';
-import Questao from './screens/Questao';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <ConteudoStack.Navigator initialRouteName="Conteudo">
-        <ConteudoStack.Screen name="Conteudo" component={Conteudo} />
-        <ConteudoStack.Screen name="Teste" component={Teste} />
-        <ConteudoStack.Screen name="Questao" component={Questao} />
-      </ConteudoStack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    backgroundColor: '#fff',
+
+    alignItems: 'center',
+
+    justifyContent: 'center',
+  },
+});
--- src/typing/generalTypes.ts
+++
@@ -1,5 +0,0 @@
-export type ListaConteudosType = {
-  id: number;
-  icon: 'book' | 'warning' | 'dashboard' | 'cloudo' | 'tool' | 'medicinebox' | 'idcard';
-  name: string;
-};
