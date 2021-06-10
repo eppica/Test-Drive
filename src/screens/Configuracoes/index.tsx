0dd37e371775163cc17c34681750d4ff53e495db
Pedro Henrique Ferreira <pedrohfcd@hotmail.com>
2021-06-10 18:34:02 -0300

    Fix components name

--- src/screens/Configuracoes/index.tsx
+++ src/screens/Configuracoes/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text } from 'react-native';
 import { ViewStyled } from './styles';
 
-export default function Questao() {
+export default function Configuracoes() {
   return (
     <ViewStyled>
       <Text>Tela Configuracao</Text>
--- src/screens/Estatisticas/index.tsx
+++ src/screens/Estatisticas/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-export default function Questao() {
+export default function Estatisticas() {
   return (
     <View>
       <Text>Tela Estatisticas</Text>
