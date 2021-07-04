25d5d7eba7337c6621f25f036edc926402e15564
Pedro Henrique Ferreira <pedrohfcd@hotmail.com>
2021-07-04 18:55:23 -0300

    Create mock json test and questions types
    Closes #11

--- src/typing/generalTypes.ts
+++ src/typing/generalTypes.ts
@@ -3,3 +3,23 @@ export type ListaConteudosType = {
   icon: 'book' | 'warning' | 'dashboard' | 'cloudo' | 'tool' | 'medicinebox' | 'idcard';
   name: string;
 };
+
+export type test = {
+  type: string,
+  quantity: number,
+  questions: string[]
+}
+
+export type alternative = {
+  index: number,
+  content: string
+}
+
+export type question = {
+  index: number,
+  type: number,
+  question: string,
+  image: string,
+  alternatives: alternative[],
+  answer: number
+}
