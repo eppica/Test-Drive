71a75230b6dcfce95f0f1282eec4947ba1a872ae
Pedro Henrique Ferreira <pedrohfcd@hotmail.com>
2021-07-03 23:04:11 -0300

    Draw configs screen
    Closes #7

--- src/screens/Configuracoes/index.tsx
+++ src/screens/Configuracoes/index.tsx
@@ -1,11 +1,76 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { ViewStyled } from './styles';
+import React, { SyntheticEvent, useState } from 'react';
+import { Switch } from 'react-native';
+import { ViewStyled, List, HideableList, Title, ConfigText, Input } from './styles';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function Configuracoes() {
+  const [isNotificationEnabled, setIsNotificationEnabled] = useState(false);
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const [notificationHour, setNotificationHour] = useState(new Date(0));
+  const [maxTestTime, setMaxTestTime] = useState('20');
+  const [questionsQuantity, setQuestionsQuantity] = useState('10');
+
+  const handleSwitch = () => setIsNotificationEnabled((previousState) => !previousState);
+
+  const handleTimeChange = (
+    event: SyntheticEvent<Readonly<{ timestamp: number }>, Event>,
+    date: Date | undefined,
+  ) => {
+    setShowTimePicker(false);
+    setNotificationHour(date || notificationHour);
+  };
+
+  const handleShowTimePicker = () => setShowTimePicker(true);
+
+  const handleMaxTestTime = (text: string) => {
+    setMaxTestTime(text);
+  };
+
+  const handleQuestionsQuantity = (text: string) => {
+    setQuestionsQuantity(text);
+  };
+
   return (
     <ViewStyled>
-      <Text>Tela Configuracao</Text>
+      <List>
+        <Title>Notificações</Title>
+        <Switch
+          trackColor={{ false: '#767577', true: '#d0cfd1' }}
+          thumbColor={isNotificationEnabled ? '#000' : '#f4f3f4'}
+          onValueChange={handleSwitch}
+          value={isNotificationEnabled}
+        />
+      </List>
+
+      {isNotificationEnabled && (
+        <HideableList>
+          <Title>Hora da notificação</Title>
+          <ConfigText onPress={handleShowTimePicker}>
+            {notificationHour.toTimeString().substr(0, 5)}
+          </ConfigText>
+          {showTimePicker && (
+            <DateTimePicker
+              value={notificationHour}
+              mode="time"
+              is24Hour={true}
+              display="default"
+              onChange={handleTimeChange}
+            />
+          )}
+        </HideableList>
+      )}
+      <List>
+        <Title>Duração dos Testes (min)</Title>
+        <Input onChangeText={handleMaxTestTime} value={maxTestTime} keyboardType="numeric" />
+      </List>
+      <List>
+        <Title>Quantidade de Questões</Title>
+        <Input
+          onChangeText={handleQuestionsQuantity}
+          value={questionsQuantity}
+          keyboardType="numeric"
+        />
+      </List>
     </ViewStyled>
   );
 }
--- src/screens/Configuracoes/styles.tsx
+++ src/screens/Configuracoes/styles.tsx
@@ -1,6 +1,37 @@
 import styled from 'styled-components';
-import { View } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
 export const ViewStyled = styled(View)`
-  background-color: red;
+  flex: 1;
+  background-color: #fff;
+  flex-direction: column;
+`;
+
+export const List = styled(View)`
+  flex-direction: row;
+  padding: 20px;
+  background-color: #fff;
+  justify-content: space-between;
+`;
+
+export const HideableList = styled(View)`
+  flex-direction: row;
+  padding: 20px;
+  background-color: #d9d7d7;
+  justify-content: space-between;
+`;
+
+export const Title = styled(Text)`
+  font-size: 18px;
+  margin-left: 20px;
+`;
+
+export const ConfigText = styled(Text)`
+  font-size: 16px;
+  margin-right: 15px;
+`;
+
+export const Input = styled(TextInput)`
+  font-size: 16px;
+  margin-right: 15px;
 `;
